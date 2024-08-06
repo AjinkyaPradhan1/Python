f2d5d6aa207ff69638bcf6287d4d8397ead746a0
Ajinkya Pradhan <101694262+AjinkyaPradhan1@users.noreply.github.com>
2024-08-06 23:03:24 +0530

    Add files via upload

---
+++ Practice/CountInstances.py
@@ -0,0 +1,12 @@
+#  Print without duplicates
+
+list1 = [21.54,45,65,54,56,54,65,76,8,9,65,32,43,12,21]
+
+list2 = []
+
+for i in list1:
+    if i not in list2:
+        list2.append(i)
+        
+
+print("List2: ",list2)
